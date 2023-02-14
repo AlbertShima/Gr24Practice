dd1ee56d2ea82417df3c5bf38e6eafd99576d07d
Albert Shima <albertshima@gmail.com>
2023-02-14 20:47:39 +0100

    Creating our framework

---
+++ src/test/java/com/gr24/utilities/ConfigurationReader.java
@@ -0,0 +1,22 @@
+package com.gr24.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Wrong file");
+        }
+    }
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+}
